fbd4b7553c0114a03a7c844768c92261fab542f6
Stefan Kairinos <118008817+SKairinos@users.noreply.github.com>
2023-03-13 15:12:34 +0000

    Initial commit

---
+++ src/pages/jokeList/JokeListPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import JokeList from 'features/jokeList/JokeList';
+import styles from './JokeListPage.module.scss';
+
+import { SayHello } from 'codeforlife';
+
+export default function JokeListPage(): JSX.Element {
+  return (
+    <div className={styles.page}>
+      <JokeList />
+      <SayHello name="Stefan" />
+    </div>
+  );
+}
