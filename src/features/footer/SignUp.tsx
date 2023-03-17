095de2e4baba8c5f15ce347ac808f75247b46e9f
SKairinos <stefan.kairinos@ocado.com>
2023-03-17 15:00:32 +0000

    add white color

--- src/features/footer/SignUp.tsx
+++ src/features/footer/SignUp.tsx
@@ -6,47 +6,63 @@ import {
   InputAdornment,
   FormControlLabel,
   Checkbox,
-  Button
+  Button,
+  ThemeProvider,
+  createTheme
 } from '@mui/material';
 import {
   EmailOutlined as EmailIcon
 } from '@mui/icons-material';
 
+import theme from 'app/theme';
+
 const SignUp: React.FC = () => (
-  <Grid container>
-    <Grid xs={12}>
-      <Typography>
-        Sign up to receive updates about Code for Life games and teaching resources.
-      </Typography>
+  <ThemeProvider theme={createTheme(theme, {
+    components: {
+      MuiTypography: {
+        styleOverrides: {
+          root: {
+            color: 'white'
+          }
+        }
+      }
+    }
+  })}>
+    <Grid container>
+      <Grid xs={12}>
+        <Typography>
+          Sign up to receive updates about Code for Life games and teaching resources.
+        </Typography>
+      </Grid>
+      <Grid xs={12}>
+        <TextField
+          required
+          id='email'
+          label='Email'
+          type='email'
+          variant='filled'
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <EmailIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+      </Grid>
+      <Grid xs={12} sm={10}>
+        <FormControlLabel
+          control={<Checkbox required style={{ color: 'white' }} />}
+          label='Please confirm that you are over 18.'
+        />
+      </Grid>
+      <Grid xs={12} sm={2}>
+        <Button type='submit' fullWidth>
+          Sign up
+        </Button>
+      </Grid>
     </Grid>
-    <Grid xs={12}>
-      <TextField
-        required
-        id='email'
-        label='Email'
-        type='email'
-        variant='filled'
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position='end'>
-              <EmailIcon />
-            </InputAdornment>
-          )
-        }}
-      />
-    </Grid>
-    <Grid xs={12} sm={10}>
-      <FormControlLabel
-        control={<Checkbox required />}
-        label='Please confirm that you are over 18.'
-      />
-    </Grid>
-    <Grid xs={12} sm={2}>
-      <Button type='submit' fullWidth>
-        Sign up
-      </Button>
-    </Grid>
-  </Grid>
+  </ThemeProvider>
 );
 
 export default SignUp;
