8c6377e13230526c58695c61c6695842184baafc
SKairinos <stefan.kairinos@ocado.com>
2023-03-20 10:35:38 +0000

    skeletons for footer pages

---
+++ src/pages/termsOfUse/TermsOfUse.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+
+import Header from 'features/header/Header';
+import Footer from 'features/footer/Footer';
+
+const TermsOfUse: React.FC = () => {
+  return (
+    <Grid container>
+      <Grid xs={12}>
+        <Header />
+      </Grid>
+      <Grid xs={12}>
+        TODO
+      </Grid>
+      <Grid xs={12}>
+        <Footer />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TermsOfUse;
