e36daa4235258bedc83ae0110f4f38ca18f1abcb
SKairinos <stefan.kairinos@ocado.com>
2023-03-22 10:42:19 +0000

    table of contents component

---
+++ src/components/TableOfContents.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Unstable_Grid2 as Grid,
+  Typography,
+  Stack,
+  Divider,
+  Link
+} from '@mui/material';
+
+const TableOfContents: React.FC<{
+  contents: Array<{ header: string, element: React.ReactElement }>
+}> = ({ contents }) => {
+  const headerRefs = contents.map(() => React.useRef<HTMLSpanElement>(null));
+  const halfLength = Math.ceil(contents.length / 2);
+
+  function handleHeaderClick(index: number): void {
+    const header = headerRefs[index].current;
+    if (header !== null) header.scrollIntoView();
+  }
+
+  function generateLinkStack(sliceStart: number, sliceEnd: number): React.ReactElement {
+    return (
+      <Stack>
+        {contents.slice(sliceStart, sliceEnd).map((content, index) => (
+          <Link
+            key={index}
+            onClick={() => { handleHeaderClick(index); }}
+          >
+            {content.header}
+          </Link>
+        ))}
+      </Stack>
+    );
+  }
+
+  return (
+    <Grid container>
+      <Grid xs={12} sm={6}>
+        {generateLinkStack(0, halfLength)}
+      </Grid>
+      <Grid xs={12} sm={6}>
+        {generateLinkStack(halfLength, contents.length)}
+      </Grid>
+      {contents.map((content, index) => (
+        <Grid key={index} xs={12}>
+          <Divider />
+          <Typography ref={headerRefs[index]} variant='h6'>
+            {index + 1}. {content.header}
+          </Typography>
+          {content.element}
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default TableOfContents;
