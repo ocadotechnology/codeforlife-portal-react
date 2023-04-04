ce4b0f60884a06ae1683e7fd7895b09809d904c6
KamilPawel <ks6g18@soton.ac.uk>
2023-04-04 11:22:01 +0100

    buttons alligned on all cards

--- src/pages/getInvolved/GetInvolved.tsx
+++ src/pages/getInvolved/GetInvolved.tsx
@@ -12,104 +12,126 @@ import { PageBanner } from "pages/aboutUs/AboutUs";
 import Github from "images/github.png";
 import Clubs from "images/clubs.png";
 import Universities from "images/universities.png";
-import { FULL_HEIGHT } from "constants/sizes";
-
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { ChevronRightRounded } from "@mui/icons-material";
+import GetInvolvedHero from "images/get_involved_hero.png";
+import { BaseSection } from "pages/aboutUs/AboutUs";
+import theme from "app/theme";
+
+const CardStyle = {
+  // this creates cards that will have matching heights
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+} as React.CSSProperties;
+
+const FirstCard: React.FC = () => {
+  return (
+    <Card sx={CardStyle}>
+      <CardMedia sx={{ height: 242 }} image={Clubs} title="Clubs" />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5">
+          Starting a coding club of your own
+        </Typography>
+        <Typography variant="body1">
+          Become a Code for Life ambassador by starting up a coding club. Find
+          out more about how you can get involved with this by visiting our
+          coding club page.
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Button variant="contained" endIcon={<ChevronRightRounded />}>
+          Read more
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+const SecondCard: React.FC = () => {
+  return (
+    <Card sx={CardStyle}>
+      <CardMedia sx={{ height: 242 }} image={Github} title="Github" />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5">
+          Contribute through code
+        </Typography>
+        <Typography variant="body1">
+          We welcome volunteers from all backgrounds to help us with our coding
+          adventure. Take a look at our contribution guide to find out how to
+          get involved in our open source projects.
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Button variant="contained" endIcon={<ChevronRightRounded />}>
+          Read more
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+const ThirdCard: React.FC = () => {
+  return (
+    <Card sx={CardStyle}>
+      <CardMedia
+        sx={{ height: 242 }}
+        image={Universities}
+        title="Universities"
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5">
+          University partnerships
+        </Typography>
+        <Typography sx={{ flexGrow: 1 }} variant="body1">
+          Please get in touch at codeforlife@ocado.com if you are interested in
+          working on Code for Life projects with your students including:
+          coding, user experience, data analytics and new feature design.
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Button variant="contained" endIcon={<ChevronRightRounded />}>
+          Read more
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+const GetInvolvedBanner: React.FC = () => {
+  return (
+    <PageBanner
+      title="Get involved"
+      description="How you can get involved with the creation of Code for Life products and resources"
+      img={{ alt: "Get involved", src: GetInvolvedHero }}
+    />
+  );
+};
 
 const GetInvolved: React.FC = () => {
   return (
     <BasePage>
-      <Container>
-        <Grid
-          spacing={1}
-          container
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="stretch"
-          p={0}
-          xs={12}
-        >
-          <Grid p="inherit" xs={12} md={6} lg={4}>
-            <Card sx={{height:"100%"}}>
-              <CardMedia sx={{ height: "16rem"}} image={Clubs} title="Clubs" />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Starting a coding club of your own
-                </Typography>
-                <Typography variant="body1">
-                  Become a Code for Life ambassador by starting up a coding
-                  club. Find out more about how you can get involved with this
-                  by visiting our coding club page.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button variant="contained" endIcon={<ChevronRightRounded />}>
-                  Read more
-                </Button>
-              </CardActions>
-            </Card>
+      <BaseSection containerProps={{ bgcolor: theme.palette.primary.main }}>
+        <GetInvolvedBanner />
+      </BaseSection>
+      <BaseSection>
+        <Grid spacing={4} container direction="row" p={4} xs={12}>
+          <Grid xs={12} md={6} lg={4}>
+            <FirstCard />
           </Grid>
           <Grid xs={12} md={6} lg={4}>
-            <Card sx={{height: "100%"}}>
-              <CardMedia sx={{ height: "16rem" }} image={Github} title="Github" />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Contribute through code
-                </Typography>
-                <Typography variant="body1">
-                  We welcome volunteers from all backgrounds to help us with our
-                  coding adventure. Take a look at our contribution guide to
-                  find out how to get involved in our open source projects.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button variant="contained" endIcon={<ChevronRightRounded />}>
-                  Read more
-                </Button>
-              </CardActions>
-            </Card>
+            <SecondCard />
           </Grid>
           <Grid xs={12} md={6} lg={4}>
-            <Card sx={{height: "100%"}}>
-              <CardMedia sx={{ height: "16rem" }} image={Universities} title="Universities" />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  University partnerships
-                </Typography>
-                <Typography variant="body1">
-                  Please get in touch at codeforlife@ocado.com if you are
-                  interested in working on Code for Life projects with your
-                  students including: coding, user experience, data analytics
-                  and new feature design.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button variant="contained" endIcon={<ChevronRightRounded />}>
-                  Read more
-                </Button>
-              </CardActions>
-            </Card>
+            <ThirdCard />
           </Grid>
         </Grid>
-      </Container>
+      </BaseSection>
     </BasePage>
   );
 };
 
 export default GetInvolved;
-/*
-
-<Card
-description="We welcome volunteers from all backgrounds to help us with our coding adventure. Take a look at our contribution guide to find out how to get involved in our open source projects."
-  img={{ alt: "Github image", src: Github }}
-  />
-<Card
-title="University partnerships"
-description="Please get in touch at codeforlife@ocado.com if you are interested in working on Code for Life projects with your students including: coding, user experience, data analytics and new feature design."
-img={{ alt: "Github image", src: Universities }}
-/>
-*/
