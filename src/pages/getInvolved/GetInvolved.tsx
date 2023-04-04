92f55f2d4238ac7cdf3315e34501e6ebf49963c2
KamilPawel <ks6g18@soton.ac.uk>
2023-04-04 17:29:47 +0100

    responded to comments

--- src/pages/getInvolved/GetInvolved.tsx
+++ src/pages/getInvolved/GetInvolved.tsx
@@ -1,33 +1,34 @@
-import React from "react";
+import React from 'react';
 import {
   Unstable_Grid2 as Grid,
   Typography,
   Button,
   Container,
-} from "@mui/material";
+  Card,
+  CardContent,
+  CardMedia
+} from '@mui/material';
 
-import BasePage from "pages/BasePage";
-import PageBanner from "components/PageBanner";
-import Github from "images/github.png";
-import Clubs from "images/clubs.png";
-import Universities from "images/universities.png";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import { ChevronRightRounded } from "@mui/icons-material";
-import GetInvolvedHero from "images/get_involved_hero.png";
-import theme from "app/theme";
-import { ImageTextProps } from "components/interfaces/interfaces";
+import BasePage from 'pages/BasePage';
+import PageBanner from 'components/PageBanner';
 
-const CardStyle = {
+import GithubImg from 'images/github.png';
+import ClubsImg from 'images/clubs.png';
+import UniversitiesImg from 'images/universities.png';
+
+import { ChevronRightRounded } from '@mui/icons-material';
+import GetInvolvedHero from 'images/get_involved_hero.png';
+import { ImageTextProps } from 'components/interfaces/interfaces';
+
+const CardStyle: React.CSSProperties = {
   // this creates cards that will have matching heights
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-start",
-} as React.CSSProperties;
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start'
+};
 
-const GetInvolvedCard: React.FC<ImageTextProps> = ({ text, img }) => {
+const CardCFL: React.FC<ImageTextProps> = ({ text, img }) => {
   return (
     <Card sx={CardStyle}>
       <CardMedia sx={{ height: 242 }} image={img.src} title={img.alt} />
@@ -50,11 +51,11 @@ const GetInvolvedBanner: React.FC = () => {
   return (
     <PageBanner
       text={{
-        title: "Get involved",
+        title: 'Get involved',
         content:
-          "How you can get involved with the creation of Code for Life products and resources",
+          'How you can get involved with the creation of Code for Life products and resources'
       }}
-      img={{ alt: "Get involved", src: GetInvolvedHero }}
+      img={{ alt: 'Get involved', src: GetInvolvedHero }}
     />
   );
 };
@@ -66,41 +67,41 @@ const GetInvolved: React.FC = () => {
       <Container>
         <Grid spacing={4} container direction="row" p={4} xs={12}>
           <Grid xs={12} md={6} lg={4}>
-            <GetInvolvedCard
+            <CardCFL
               text={{
-                title: "Starting a coding club of your own",
+                title: 'Starting a coding club of your own',
                 content:
-                  "Become a Code for Life ambassador by starting up a coding club. Find out more about how you can get involved with this by visiting our coding club page.",
+                  'Become a Code for Life ambassador by starting up a coding club. Find out more about how you can get involved with this by visiting our coding club page.'
               }}
               img={{
-                alt: "Clubs",
-                src: Clubs,
+                alt: 'Clubs',
+                src: ClubsImg
               }}
             />
           </Grid>
           <Grid xs={12} md={6} lg={4}>
-            <GetInvolvedCard
+            <CardCFL
               text={{
-                title: "Contribute through code",
+                title: 'Contribute through code',
                 content:
-                  "We welcome volunteers from all backgrounds to help us with our coding adventure. Take a look at our contribution guide to find out how to get involved in our open source projects.",
+                  'We welcome volunteers from all backgrounds to help us with our coding adventure. Take a look at our contribution guide to find out how to get involved in our open source projects.'
               }}
               img={{
-                alt: "Github",
-                src: Github,
+                alt: 'Github',
+                src: GithubImg
               }}
             />
           </Grid>
           <Grid xs={12} md={6} lg={4}>
-            <GetInvolvedCard
+            <CardCFL
               text={{
-                title: "University partnerships",
+                title: 'University partnerships',
                 content:
-                  "Please get in touch at codeforlife@ocado.com if you are interested in working on Code for Life projects with your students including coding, user experience, data analytics and new feature design.",
+                  'Please get in touch at codeforlife@ocado.com if you are interested in working on Code for Life projects with your students including coding, user experience, data analytics and new feature design.'
               }}
               img={{
-                alt: "Universities",
-                src: Universities,
+                alt: 'Universities',
+                src: UniversitiesImg
               }}
             />
           </Grid>
