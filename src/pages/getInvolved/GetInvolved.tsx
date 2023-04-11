efedee296952c88eedfc6a794860a39939adbc14
KamilPawel <ks6g18@soton.ac.uk>
2023-04-11 09:17:13 +0100

    fix: button card not aligned

--- src/pages/getInvolved/GetInvolved.tsx
+++ src/pages/getInvolved/GetInvolved.tsx
@@ -8,9 +8,7 @@ import {
   CardMedia,
   CardActions
 } from '@mui/material';
-import {
-  ChevronRightRounded as ChevronRightRoundedIcon
-} from '@mui/icons-material';
+import { ChevronRightRounded as ChevronRightRoundedIcon } from '@mui/icons-material';
 
 import { ImageProps } from 'codeforlife/lib/esm/components';
 
@@ -24,30 +22,26 @@ import UniversitiesImg from 'images/universities.png';
 import GetInvolvedHero from 'images/get_involved_hero_cut.png';
 
 const ReadMoreCard: React.FC<{
-  text: { title: string, content: string },
-  img: ImageProps
+  text: { title: string; content: string };
+  img: ImageProps;
 }> = ({ text, img }) => {
   return (
     <Grid xs={12} md={6} lg={4}>
-      <Card style={{ height: '100%' }}>
+      <Card
+        style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+      >
         <CardMedia
-          component='img'
+          component="img"
           height={242}
           image={img.src}
           title={img.alt}
         />
-        <CardContent>
-          <Typography variant='h5'>
-            {text.title}
-          </Typography>
-          <Typography>
-            {text.content}
-          </Typography>
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography variant="h5">{text.title}</Typography>
+          <Typography>{text.content}</Typography>
         </CardContent>
         <CardActions>
-          <Button endIcon={<ChevronRightRoundedIcon />}>
-            Read more
-          </Button>
+          <Button endIcon={<ChevronRightRoundedIcon />}>Read more</Button>
         </CardActions>
       </Card>
     </Grid>
@@ -60,7 +54,8 @@ const GetInvolved: React.FC = () => {
       <PageBanner
         text={{
           title: 'Get involved',
-          content: 'How you can get involved with the creation of Code for Life products and resources'
+          content:
+            'How you can get involved with the creation of Code for Life products and resources'
         }}
         img={{ alt: 'Get involved', src: GetInvolvedHero }}
       />
@@ -69,21 +64,24 @@ const GetInvolved: React.FC = () => {
           <ReadMoreCard
             text={{
               title: 'Starting a coding club of your own',
-              content: 'Become a Code for Life ambassador by starting up a coding club. Find out more about how you can get involved with this by visiting our coding club page.'
+              content:
+                'Become a Code for Life ambassador by starting up a coding club. Find out more about how you can get involved with this by visiting our coding club page.'
             }}
             img={{ alt: 'Clubs', src: ClubsImg }}
           />
           <ReadMoreCard
             text={{
               title: 'Contribute through code',
-              content: 'We welcome volunteers from all backgrounds to help us with our coding adventure. Take a look at our contribution guide to find out how to get involved in our open source projects.'
+              content:
+                'We welcome volunteers from all backgrounds to help us with our coding adventure. Take a look at our contribution guide to find out how to get involved in our open source projects.'
             }}
             img={{ alt: 'Github', src: GithubImg }}
           />
           <ReadMoreCard
             text={{
               title: 'University partnerships',
-              content: 'Please get in touch at codeforlife@ocado.com if you are interested in working on Code for Life projects with your students including coding, user experience, data analytics and new feature design.'
+              content:
+                'Please get in touch at codeforlife@ocado.com if you are interested in working on Code for Life projects with your students including coding, user experience, data analytics and new feature design.'
             }}
             img={{ alt: 'Universities', src: UniversitiesImg }}
           />
