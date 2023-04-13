8de331ea841fb855001153864192439ed84f8031
SKairinos <stefan.kairinos@ocado.com>
2023-04-13 18:25:48 +0100

    use reusable component from cfl package

--- src/pages/students/Characters.tsx
+++ src/pages/students/Characters.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {
   Unstable_Grid2 as Grid,
-  Grid2Props,
   Typography
 } from '@mui/material';
 
-import { Image, ImageProps } from 'codeforlife/lib/esm/components';
+import {
+  Image,
+  ImageProps,
+  OrderedGrid
+} from 'codeforlife/lib/esm/components';
 
 const Characters: React.FC<{
   characters: Array<{
@@ -15,48 +18,7 @@ const Characters: React.FC<{
   }>,
   imageMaxHeight: string
 }> = ({ characters, imageMaxHeight }) => {
-  const images = characters.map(c => c.image);
-  const names = characters.map(c => c.name);
-  const descriptions = characters.map(c => c.description);
-
-  const getProps = (
-    itemIndex: number, rowIndex: number
-  ): Grid2Props => {
-    const xs = 12;
-    const sm = 6;
-    const md = xs / characters.length;
-
-    const getColsPerRow = (size: number): number => Math.floor(12 / size);
-
-    const getOrder = (size: number): number => (
-      (Math.floor(itemIndex / getColsPerRow(size)) * 3) + rowIndex
-    );
-
-    const getOffset = (size: number): number => {
-      const colsPerRow = getColsPerRow(size);
-      return (
-        characters.length % colsPerRow !== 0 &&
-        // only apply offset to last item
-        itemIndex === characters.length - 1
-      )
-        ? size / colsPerRow
-        : 0;
-    };
-
-    return {
-      xs,
-      sm,
-      md,
-      xsOffset: getOffset(xs),
-      smOffset: getOffset(sm),
-      mdOffset: getOffset(md),
-      order: {
-        xs: getOrder(xs),
-        sm: getOrder(sm),
-        md: getOrder(md)
-      }
-    };
-  };
+  const md = 12 / characters.length;
 
   return (
     <Grid container>
@@ -65,44 +27,45 @@ const Characters: React.FC<{
           Meet the characters
         </Typography>
       </Grid>
-      {images.map((image, index) => (
-        <Grid
-          key={`image-${index}`}
-          className='flex-center'
-          {...getProps(index, 0)}
-        >
-          <Image
-            {...image}
-            boxProps={{
-              style: {
-                width: 'auto',
-                height: 'auto',
-                maxHeight: imageMaxHeight
-              }
-            }}
-          />
-        </Grid>
-      ))}
-      {names.map((name, index) => (
-        <Grid
-          key={`name-${index}`}
-          {...getProps(index, 1)}
-        >
-          <Typography variant='h5'>
-            {name}
-          </Typography>
-        </Grid>
-      ))}
-      {descriptions.map((description, index) => (
-        <Grid
-          key={`description-${index}`}
-          {...getProps(index, 2)}
-        >
-          <Typography>
-            {description}
-          </Typography>
-        </Grid>
-      ))}
+      <OrderedGrid
+        rows={[
+          characters.map((character, index) => ({
+            element: (
+              <Image
+                key={index}
+                style={{
+                  width: 'auto',
+                  height: 'auto',
+                  maxHeight: imageMaxHeight
+                }}
+                {...character.image}
+              />
+            ),
+            itemProps: { className: 'flex-center' }
+          })),
+          characters.map((character, index) => ({
+            element: (
+              <Typography key={index} variant='h5'>
+                {character.name}
+              </Typography>
+            )
+          })),
+          characters.map((character, index) => ({
+            element: (
+              <Typography key={index}>
+                {character.description}
+              </Typography>
+            )
+          }))
+        ]}
+        globalItemProps={{
+          xs: 12,
+          sm: 6,
+          md,
+          lg: md,
+          xl: md
+        }}
+      />
     </Grid>
   );
 };
