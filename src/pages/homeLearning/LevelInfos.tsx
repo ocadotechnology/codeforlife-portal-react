f390188bdcd797bf516aed96a6f6f406e152e9f1
cewei8483 <cewei8483@gmail.com>
2023-04-14 13:50:30 +0100

    beginner level

--- src/pages/homeLearning/LevelInfos.tsx
+++ src/pages/homeLearning/LevelInfos.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
   Container,
   Unstable_Grid2 as Grid,
   GridDirection,
@@ -7,12 +12,53 @@ import {
 } from '@mui/material';
 
 import { ResponsiveStyleValue } from '@mui/system';
-import { Image } from 'codeforlife/lib/esm/components';
+import { ImageProps } from 'codeforlife/lib/esm/components';
 import RRBeginnerImage from 'images/rr_beginner.png';
+
+const ReadMoreCard: React.FC<{
+  text: { title: string; content: string };
+  img: ImageProps;
+}> = ({ text, img }) => {
+  return (
+    <>
+      <Card
+        style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+      >
+        <CardMedia
+          component="img"
+          height={242}
+          image={img.src}
+          title={img.alt}
+        />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography variant="h5">{text.title}</Typography>
+          <Typography>{text.content}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button>Read more</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
+
+const Sessions: React.FC<{
+  title: string,
+  children: React.ReactNode,
+}> = ({ title, children }) => {
+  return <>
+    <Typography variant='h6'>
+      {title}
+    </Typography>
+    <Typography>
+      {children}
+    </Typography>
+  </>;
+};
 
 const LevelInfo: React.FC<{
   level: { difficulty: string, textcolor: string, bgcolor: string },
-  card: { img: { alt: string, src: string }, description: string }
+  card: { img: { alt: string, src: string }, text: { title: string, content: string } }
   children: React.ReactNode,
   direction?: ResponsiveStyleValue<GridDirection>
 }> = ({ level, card, children, direction }) => {
@@ -24,26 +70,31 @@ const LevelInfo: React.FC<{
         </Typography>
       </Container>
     </Grid>
-    <Grid
-      container
-      spacing={{ xs: 2, lg: 3 }}
-      display='flex'
-      direction={direction}
-    >
-      <Grid xs={12} md={6}>
-        {children}
-      </Grid>
-      <Grid xs={12} md={6} className='flex-center'>
-        <Image
-          alt={card.img.alt}
-          src={card.img.src}
-        />
-      </Grid>
-    </Grid>
+    <Container maxWidth='lg'>
+      <Grid
+        container
+        spacing={{ xs: 2, lg: 4 }}
+        display='flex'
+        direction={direction}
+        paddingY={5}
+      >
+        <Grid xs={12} md={4}>
+          <ReadMoreCard
+            img={{ alt: card.img.alt, src: card.img.src }}
+            text={{ title: card.text.title, content: card.text.content }}
+          />
+        </Grid>
+        <Grid xs={12} md={8}>
+          {children}
+        </Grid>
+      </Grid >
+    </Container>
   </>;
 };
 
 const LevelInfos: React.FC = () => {
+  const BeginnerContent = 'Teach your child about problem-solving and logical reasoning as they play. They\'ll explore algorithms, and learn how to create and debug simple programs. Designed for children aged 5-7, but start here if you\'ve never played Rapid Router.';
+
   return <>
     <LevelInfo
       level={{
@@ -53,11 +104,25 @@ const LevelInfos: React.FC = () => {
       }}
       card={{
         img: { alt: 'RR beginner image', src: RRBeginnerImage },
-        description: 'rr begin'
+        text: { title: 'Beginner', content: BeginnerContent }
       }}
     >
-      RR beginner
-    </LevelInfo>
+      <Typography variant='h5'>
+        Levels 1-16
+      </Typography>
+      <Sessions title='Session 1'>
+        Print off the worksheets, and cut them out to play offline. This helps children see that they need to give the computer clear instructions so it can understand how to move the van.
+      </Sessions>
+      <Sessions title='Session 2'>
+        Explore Rapid Router online and learn how to play. Print the left-right van on page 6, to help avoid any confusion between left and right. Once a child has understood the basics, they can work on levels 1 to 5.
+      </Sessions>
+      <Sessions title='Session 3'>
+        Plan longer routes with more turns to learn how to create more complex algorithms. The direct drive buttons make things a little easier for younger children, or those with learning difficulties while they build confidence.
+      </Sessions>
+      <Sessions title='Session 4 & 5'>
+        Print off worksheets focusing on efficient routes and planning using levels 13 and 14. Levels 15-16 include routes where there is more than one delivery to make and some complex and tangled routes.
+      </Sessions>
+    </LevelInfo >
   </>;
 };
 
