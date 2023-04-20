a8892b8e43f8a1e69bda2421f16eb85f5b906db7
SKairinos <stefan.kairinos@ocado.com>
2023-04-20 12:05:25 +0100

    reuse component from cfl package

--- src/pages/home/TargetAudience.tsx
+++ src/pages/home/TargetAudience.tsx
@@ -2,94 +2,91 @@ import React from 'react';
 import {
   Unstable_Grid2 as Grid,
   Typography,
-  Button
+  Button,
+  ButtonProps
 } from '@mui/material';
 import {
   ChevronRight as ChevronRightIcon
 } from '@mui/icons-material';
 
-import { Image } from 'codeforlife/lib/esm/components';
+import { Image, ImageProps, OrderedGrid } from 'codeforlife/lib/esm/components';
 
 import { paths } from 'app/router';
 import EducateImage from 'images/dashboard_educate.png';
 import PlayImage from 'images/dashboard_play.png';
 
-const Content: React.FC<{
-  index: number
-  img: { alt: string, src: string }
-  text: { header: string, body: string }
-  btn: { text: string, href: string }
-}> = ({ index, img, text, btn }) => <>{
-  [
-    {
-      props: { className: 'flex-center' },
-      children: (
-        <Image
-          alt={img.alt}
-          src={img.src}
-          maxWidth='450px'
-        />
-      )
-    },
-    {
-      children: (
-        <Typography
-          variant='h2'
-          style={{ color: 'white' }}
-        >
-          {text.header}
-        </Typography>
-      )
-    },
-    {
-      children: (
-        <Typography
-          fontSize={21}
-          style={{ color: 'white' }}
-        >
-          {text.body}
-        </Typography>
-      )
-    },
-    {
-      props: { className: 'flex-end' },
-      children: (
-        <Button
-          color='white'
-          href={btn.href}
-          endIcon={<ChevronRightIcon />}
-          sx={{ mb: { xs: 2, sm: 0 } }}
-        >
-          {btn.text}
-        </Button>
-      )
-    }
-  ].map((grid, gridIndex) => <>
-    <Grid
-      xs={12} sm={6}
-      order={{ sm: (gridIndex * 2) + 1 + index }}
-      {...grid.props}
-    >
-      {grid.children}
-    </Grid>
-  </>)
-}</>;
+const TargetAudience: React.FC = () => {
+  const images: ImageProps[] = [
+    { alt: 'teacher with student', src: EducateImage },
+    { alt: 'kids playing', src: PlayImage }
+  ];
+  const headers: string[] = [
+    'Educate',
+    'Play'
+  ];
+  const bodies: string[] = [
+    'Helping teachers and families to inspire the next generation of computer scientists.',
+    'Anyone can learn how to code. We will help you learn how. It\'s fun, free and easy.'
+  ];
+  const buttons: ButtonProps[] = [
+    { children: 'Learn more', href: paths.teachers },
+    { children: 'Get started', href: paths.students }
+  ];
 
-const TargetAudience: React.FC = () => <>
-  <Grid container columnSpacing={4}>
-    <Content
-      index={0}
-      img={{ alt: 'teacher with student', src: EducateImage }}
-      text={{ header: 'Educate', body: 'Helping teachers and families to inspire the next generation of computer scientists.' }}
-      btn={{ text: 'Learn more', href: paths.teachers }}
+  return (
+    <OrderedGrid
+      containerProps={{ columnSpacing: 4 }}
+      rows={[
+        images.map((image) => ({
+          element: (
+            <Image
+              {...image}
+              maxWidth='450px'
+            />
+          ),
+          itemProps: { className: 'flex-center' }
+        })),
+        headers.map((header) => ({
+          element: (
+            <Typography
+              variant='h2'
+              style={{ color: 'white' }}
+            >
+              {header}
+            </Typography>
+          )
+        })),
+        bodies.map((body) => ({
+          element: (
+            <Typography
+              fontSize={21}
+              style={{ color: 'white' }}
+            >
+              {body}
+            </Typography>
+          )
+        })),
+        buttons.map((button) => ({
+          element: (
+            <Button
+              color='white'
+              endIcon={<ChevronRightIcon />}
+              sx={{ mb: { xs: 2, sm: 0 } }}
+              {...button}
+            />
+          ),
+          itemProps: { className: 'flex-end' }
+        }))
+      ]}
+      globalItemProps={{
+        xs: 12,
+        sm: 6,
+        md: 6,
+        lg: 6,
+        xl: 6
+      }}
     />
-    <Content
-      index={1}
-      img={{ alt: 'kids playing', src: PlayImage }}
-      text={{ header: 'Play', body: "Anyone can learn how to code. We will help you learn how. It's fun, free and easy." }}
-      btn={{ text: 'Get started', href: paths.students }}
-    />
-  </Grid>
-</>;
+  );
+};
 
 export default TargetAudience;
