1de80ecf2336ce8d7eb430405c58b281438a62e5
cewei8483 <cewei8483@gmail.com>
2023-04-28 19:23:12 +0100

    rewrite teacher form with formik & yup

--- src/pages/register/TeacherRegister.tsx
+++ src/pages/register/TeacherRegister.tsx
@@ -11,85 +11,156 @@ import {
   Button,
   FormControlLabel,
   Checkbox,
-  FormControl
+  FormControl,
+  InputAdornment
 } from '@mui/material';
 import { ChevronRightRounded as ChevronRightRoundedIcon } from '@mui/icons-material';
-
+import SecurityIcon from '@mui/icons-material/Security';
 import { paths } from 'app/router';
 import Password from './Password';
 import { useNavigate } from 'react-router-dom';
 
+import { Formik, Field, Form, FormikHelpers, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import PasswordStatus from './PasswordStatus';
+
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+  termsOfUse: boolean;
+  receiveUpdates: boolean;
+  password: string;
+  repeatPassword: string;
+}
+
+const TeacherFormSchema = Yup.object({
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email address').required('Required'),
+  termsOfUse: Yup.bool().oneOf([true], 'You need to accept the terms and conditions'),
+  // TODO: strong password test
+  password: Yup.string().required('Field is required'),
+  repeatPassword: Yup.string().oneOf([Yup.ref('password'), undefined], "Passwords don't match").required('Confirm Password is required'),
+})
+
 const TeacherForm: React.FC = () => {
   const [pwd, setPwd] = useState('');
-  const [pwdMatch, setPwdMatch] = useState(true);
   const [isStrongPwd, setIsStrongPwd] = useState(false);
 
-  const navigate = useNavigate();
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    if (!pwdMatch) {
-      alert('Passwords do not match!');
-    }
-    if (pwdMatch && isStrongPwd) {
-      navigate(paths.emailVerificationSent, { state: { isTeacher: true } })
-    }
-  }
+  const onChangePassword = (ev: React.ChangeEvent<HTMLInputElement>): void => {
+    setPwd(ev.target.value);
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl>
-        <TextField id='first-name'
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        termsOfUse: false,
+        receiveUpdates: false,
+        password: '',
+        repeatPassword: '',
+      }}
+      validationSchema={TeacherFormSchema}
+      onSubmit={(
+        values: Values,
+        { setSubmitting }: FormikHelpers<Values>
+      ) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      <Form>
+        <Field
+          id='firstName'
+          name='firstName'
           placeholder='First name'
-          variant='outlined'
+          as={TextField}
           size='small'
-          required
         />
         <Typography paddingY={1}>
           Enter your first name
         </Typography>
+        <ErrorMessage name="firstName">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
 
-        <TextField id='last-name'
+        <Field
+          id='lastName'
+          name='lastName'
           placeholder='Last name'
-          variant='outlined'
+          as={TextField}
           size='small'
-          required
         />
         <Typography paddingY={1}>
           Enter your last name
         </Typography>
+        <ErrorMessage name="lastName">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
 
-        <TextField id='email-address'
-          type='email'
+        <Field
+          id='email'
+          name='email'
           placeholder='Email address'
-          variant='outlined'
+          as={TextField}
           size='small'
-          required
         />
         <Typography paddingY={1}>
           Enter your email address
         </Typography>
+        <ErrorMessage name="email">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
 
-        <FormControlLabel
-          control={<Checkbox required style={{ color: 'white' }} />}
-          label={<>I am over 18 years old have read and understood
-            the <Link href={paths.termsOfUse} color='inherit' underline='always' target='_blank'>Terms of use</Link>
-            &nbsp;and the <Link href={paths.privacyNotice} color='inherit' underline='always' target='_blank'>Privacy notice</Link>.
-          </>}
+        <Typography>
+          <Field type='checkbox' name='termsOfUse' />
+          &nbsp;I am over 18 years old have read and understood
+          the <Link href={paths.termsOfUse} color='inherit' underline='always' target='_blank'>Terms of use</Link>
+          &nbsp;and the <Link href={paths.privacyNotice} color='inherit' underline='always' target='_blank'>Privacy notice</Link>.
+        </Typography>
+        <ErrorMessage name="termsOfUse">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
+
+        <Typography paddingY={1}>
+          <Field type='checkbox' name='receiveUpdates' />
+          &nbsp;Sign up to receive updates about Code for Life games and teaching resources.
+        </Typography>
+
+        <Field
+          id='password'
+          name='password'
+          placeholder='Password'
+          as={TextField}
+          size='small'
+          type='password'
+          onKeyUp={onChangePassword}
+          InputProps={{
+            endAdornment: <InputAdornment position='end'><SecurityIcon /></InputAdornment>
+          }}
         />
-        <br />
+        <Typography paddingY={1}>
+          Enter a password
+        </Typography>
+        <ErrorMessage name="password">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
 
-        <FormControlLabel
-          control={<Checkbox style={{ color: 'white' }} />}
-          label='Sign up to receive updates about Code for Life games and teaching resources.'
+
+        <Field
+          id='repeatPassword'
+          name='repeatPassword'
+          placeholder='Repeat password'
+          as={TextField}
+          size='small'
+          type='password'
+          InputProps={{
+            endAdornment: <InputAdornment position='end'><SecurityIcon /></InputAdornment>
+          }}
         />
+        <Typography paddingTop={1}>
+          Repeat password
+        </Typography>
+        <ErrorMessage name="repeatPassword">{msg => <Typography>{msg}</Typography>}</ErrorMessage>
 
-        <br />
-        <Password
+        <PasswordStatus
           isTeacher={true}
           pwd={pwd}
-          setPwd={setPwd}
-          pwdMatch={pwdMatch}
-          setPwdMatch={setPwdMatch}
           setIsStrongPwd={setIsStrongPwd}
         />
 
@@ -101,8 +172,8 @@ const TeacherForm: React.FC = () => {
             Register
           </Button>
         </Grid>
-      </FormControl>
-    </form>
+      </Form>
+    </Formik>
   );
 };
 
