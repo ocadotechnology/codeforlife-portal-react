9b775b0f46c1a81f26b1b755519c5a25784cb8bb
KamilPawel <ks6g18@soton.ac.uk>
2023-05-11 11:24:00 +0100

    merge main

--- frontend/src/pages/emailVerification/EmailVerification.tsx
+++ frontend/src/pages/emailVerification/EmailVerification.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import {
-  getSearchParams,
-  stringToBoolean
-} from 'codeforlife/lib/esm/helpers';
+import { getSearchParams, stringToBoolean } from 'codeforlife/lib/esm/helpers';
 
 import SadFaceImg from '../../images/sadface.png';
 import PaperPlaneImg from '../../images/paper_plane.png';
@@ -29,34 +26,36 @@ const EmailVerification: React.FC = () => {
 
   return (
     <BasePage>
-      <PageSection maxWidth='md' className='flex-center'>
-        {params !== null && (params.success
-          ? <Status
-            forTeacher={params.forTeacher}
-            header='We need to verify your email address'
-            body={[
-              'An email has been sent to the address you provided.',
-              'Please follow the link within the email to verify your details. This will expire in 1 hour.',
-              'If you don\'t receive the email within the next few minutes, check your spam folder.'
-            ]}
-            imageProps={{
-              alt: 'PaperPlane',
-              src: PaperPlaneImg
-            }}
-          />
-          : <Status
-            forTeacher={params.forTeacher}
-            header='Your email address verification failed'
-            body={[
-              'You used an invalid link, either you mistyped the URL or that link is expired.',
-              'When you next attempt to log in, you will be sent a new verification email.'
-            ]}
-            imageProps={{
-              alt: 'SadFace',
-              src: SadFaceImg
-            }}
-          />
-        )}
+      <PageSection maxWidth="md" className="flex-center">
+        {params !== null &&
+          (params.success ? (
+            <Status
+              forTeacher={params.forTeacher}
+              header="We need to verify your email address"
+              body={[
+                'An email has been sent to the address you provided.',
+                'Please follow the link within the email to verify your details. This will expire in 1 hour.',
+                "If you don't receive the email within the next few minutes, check your spam folder."
+              ]}
+              imageProps={{
+                alt: 'PaperPlane',
+                src: PaperPlaneImg
+              }}
+            />
+          ) : (
+            <Status
+              forTeacher={params.forTeacher}
+              header="Your email address verification failed"
+              body={[
+                'You used an invalid link, either you mistyped the URL or that link is expired.',
+                'When you next attempt to log in, you will be sent a new verification email.'
+              ]}
+              imageProps={{
+                alt: 'SadFace',
+                src: SadFaceImg
+              }}
+            />
+          ))}
       </PageSection>
     </BasePage>
   );
