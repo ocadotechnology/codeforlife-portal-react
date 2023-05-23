e2c32af146634d87563727ab8542c15fc4726e17
SKairinos <stefan.kairinos@ocado.com>
2023-05-23 11:34:44 +0100

    update page tab bar

--- frontend/src/pages/privacyNotice/PrivacyNotice.tsx
+++ frontend/src/pages/privacyNotice/PrivacyNotice.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import {
-  Unstable_Grid2 as Grid
-} from '@mui/material';
 
 import BasePage from '../../pages/BasePage';
-import TabBar from '../../components/TabBar';
+import PageTabBar from '../../components/PageTabBar';
 
 import ForAdults from './ForAdults/ForAdults';
 import ForChildren from './ForChildren/ForChildren';
@@ -12,21 +9,19 @@ import ForChildren from './ForChildren/ForChildren';
 const PrivacyNotice: React.FC = () => {
   return (
     <BasePage>
-      <Grid xs={12}>
-        <TabBar
-          title='Privacy notice'
-          tabs={[
-            {
-              label: 'Privacy notice',
-              element: <ForAdults />
-            },
-            {
-              label: 'Child-friendly',
-              element: <ForChildren />
-            }
-          ]}
-        />
-      </Grid>
+      <PageTabBar
+        title='Privacy notice'
+        tabs={[
+          {
+            label: 'Privacy notice',
+            element: <ForAdults />
+          },
+          {
+            label: 'Child-friendly',
+            element: <ForChildren />
+          }
+        ]}
+      />
     </BasePage>
   );
 };
--- frontend/src/pages/termsOfUse/TermsOfUse.tsx
+++ frontend/src/pages/termsOfUse/TermsOfUse.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
-import {
-  Unstable_Grid2 as Grid
-} from '@mui/material';
 
 import BasePage from '../../pages/BasePage';
-import TabBar from 'components/TabBar';
+import PageTabBar from '../../components/PageTabBar';
 import ForAdults from './ForAdults/ForAdults';
 import ForChildren from './ForChildren/ForChildren';
 
 const TermsOfUse: React.FC = () => {
   return (
     <BasePage>
-      <Grid xs={12}>
-        <TabBar
-          title='Terms of use'
-          tabs={[
-            {
-              label: 'Terms of use',
-              element: <ForAdults />
-            },
-            {
-              label: 'Child-friendly',
-              element: <ForChildren />
-            }
-          ]}
-        />
-      </Grid>
+      <PageTabBar
+        title='Terms of use'
+        tabs={[
+          {
+            label: 'Terms of use',
+            element: <ForAdults />
+          },
+          {
+            label: 'Child-friendly',
+            element: <ForChildren />
+          }
+        ]}
+      />
     </BasePage>
   );
 };
