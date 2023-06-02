4bb2aab9c60d9c519bb10120d3e61365f47f3654
SKairinos <skairinos@gmail.com>
2023-06-02 14:34:48 +0100

    remove todo

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -141,7 +141,6 @@ const router = createBrowserRouter([
     path: paths.newsletter._,
     element: <Newsletter />
   },
-  // TODO: merge separate error pages into one page with multiple states.
   {
     path: paths.error._,
     element: <Error />
