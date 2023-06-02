f7ad2e89cb9fb8cafe60fc218e4cf1d0231ff404
cewei8483 <cewei8483@gmail.com>
2023-06-02 16:16:27 +0100

    remove props and fragments

--- frontend/src/features/ScreenTimePopUp.tsx
+++ frontend/src/features/ScreenTimePopUp.tsx
@@ -11,34 +11,30 @@ import BrainImage from '../images/brain.svg';
 const ScreenTimePopUp: React.FC<{
   open: boolean,
   setOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  interval: number
-}> = ({ open, setOpen, interval }) => {
+}> = ({ open, setOpen }) => {
   const handleClose = (): void => {
     setOpen(false);
-    setTimeout(() => { setOpen(true); }, interval);
   };
 
   return (
-    <>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        maxWidth='sm'
-      >
-        <Stack alignItems='center' margin={3}>
-          <Image src={BrainImage} alt={'brain'} maxWidth={100} marginY={3} />
-          <Typography variant='h5' textAlign='center'>
-            Time for a break?
-          </Typography>
-          <Typography textAlign='center'>
-            You have been using the Code for Life website for a while. Remember to take regular screen breaks to recharge those brain cells!
-          </Typography>
-          <Button onClick={handleClose} autoFocus>
-            Continue
-          </Button>
-        </Stack>
-      </Dialog>
-    </>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth='sm'
+    >
+      <Stack alignItems='center' margin={3}>
+        <Image src={BrainImage} alt={'brain'} maxWidth={100} marginY={3} />
+        <Typography variant='h5' textAlign='center'>
+          Time for a break?
+        </Typography>
+        <Typography textAlign='center'>
+          You have been using the Code for Life website for a while. Remember to take regular screen breaks to recharge those brain cells!
+        </Typography>
+        <Button onClick={handleClose} autoFocus>
+          Continue
+        </Button>
+      </Stack>
+    </Dialog>
   );
 };
 
--- frontend/src/features/SessionPopUp.tsx
+++ frontend/src/features/SessionPopUp.tsx
@@ -9,42 +9,39 @@ import {
 const SessionPopUp: React.FC<{
   open: boolean,
   setOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  interval: number
-}> = ({ open, setOpen, interval }) => {
+}> = ({ open, setOpen }) => {
   const minute = '2';
   const second = '00';
   const handleClose = (): void => {
     setOpen(false);
-    setTimeout(() => { setOpen(true); }, interval);
+    // TODO: close the popup after 2 mins and call the logout endpoint
   };
 
   return (
-    <>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        maxWidth='sm'
-      >
-        <Stack alignItems='center' margin={3}>
-          <Typography variant='h5' textAlign='center'>
-            Where did you go? 👀
-          </Typography>
-          <Typography textAlign='center'>
-            We noticed that you have been inactive for a while. Are you still there? For your
-            online safety we will log you out in:
-          </Typography>
-          <Typography variant='h5' textAlign='center'>
-            {minute} min {second} secs
-          </Typography>
-          <Typography textAlign='center'>
-            You may lose progress unless you continue or save.
-          </Typography>
-          <Button onClick={handleClose} autoFocus>
-            Wait, I&apos;m still here!
-          </Button>
-        </Stack>
-      </Dialog>
-    </>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth='sm'
+    >
+      <Stack alignItems='center' margin={3}>
+        <Typography variant='h5' textAlign='center'>
+          Where did you go? 👀
+        </Typography>
+        <Typography textAlign='center'>
+          We noticed that you have been inactive for a while. Are you still there? For your
+          online safety we will log you out in:
+        </Typography>
+        <Typography variant='h5' textAlign='center'>
+          {minute} min {second} secs
+        </Typography>
+        <Typography textAlign='center'>
+          You may lose progress unless you continue or save.
+        </Typography>
+        <Button onClick={handleClose} autoFocus>
+          Wait, I&apos;m still here!
+        </Button>
+      </Stack>
+    </Dialog>
   );
 };
 
