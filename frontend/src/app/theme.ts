66e21a737464ed32c1f9a949d4e1ec54f164bc04
SKairinos <skairinos@gmail.com>
2023-06-22 11:02:52 +0100

    install new cfl package with app dialogs

--- frontend/src/app/theme.ts
+++ frontend/src/app/theme.ts
@@ -8,18 +8,7 @@ import { themeOptions as cflThemeOptions } from 'codeforlife/lib/esm/theme';
 
 // Unpack the base options to extend the theme
 export const themeOptions: ThemeOptions = {
-  ...cflThemeOptions,
-  components: {
-    ...cflThemeOptions.components,
-    MuiAccordion: {
-      styleOverrides: {
-        root: {
-          margin: '0px !important',
-          borderRadius: '0px !important'
-        }
-      }
-    }
-  }
+  ...cflThemeOptions
 };
 
 const theme = responsiveFontSizes(createTheme(themeOptions));
