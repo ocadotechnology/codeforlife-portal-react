56522430e24627b534dc4f66444d737b4906e482
SKairinos <skairinos@gmail.com>
2023-06-23 19:40:53 +0100

    use wsgi app

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 # This is the entrypoint to our app.
 # https://cloud.google.com/appengine/docs/standard/python3/runtime#application_startup
-from service.asgi import application as app
+from service.wsgi import application as app
