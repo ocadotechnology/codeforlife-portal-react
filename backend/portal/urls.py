9d3a37f61c0438e20f8f3bc72825909a42bcd656
SKairinos <skairinos@gmail.com>
2023-07-12 13:24:09 +0100

    initial

--- backend/portal/urls.py
+++ backend/portal/urls.py
@@ -2,9 +2,35 @@ from django.conf.urls import url
 
 from portal.views.views import render_react
 from portal.views.dotmailer import dotmailer_consent_form, process_newsletter_form
+from .views import registration
 
 urlpatterns = [
     url(r"^api/news_signup/$", process_newsletter_form, name="process_newsletter_form"),
     url(r"^api/consent_form/$", dotmailer_consent_form, name="consent_form"),
+    url(
+        r"^user/password/reset/student/$",
+        registration.student_password_reset,
+        name="student_password_reset",
+    ),
+    url(
+        r"^user/password/reset/teacher/$",
+        registration.teacher_password_reset,
+        name="teacher_password_reset",
+    ),
+    url(
+        r"^user/password/reset/done/$",
+        registration.password_reset_done,
+        name="reset_password_email_sent",
+    ),
+    url(
+        r"^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$",
+        registration.password_reset_check_and_confirm,
+        name="password_reset_check_and_confirm",
+    ),
+    url(
+        r"^delete/account/$",
+        registration.delete_account,
+        name="delete_account",
+    ),
     url(r".*", render_react, name="react_app"),
 ]
