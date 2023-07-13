c63de92b82e33d9e23f2f5d8cb5a019e27b63992
cewei8483 <cewei8483@gmail.com>
2023-07-13 14:19:37 +0100

    update url

--- backend/portal/urls.py
+++ backend/portal/urls.py
@@ -7,7 +7,7 @@ from portal.helpers.regexes import JWT_REGEX
 
 urlpatterns = [
     url(rf"^api/verify_email/(?P<token>{JWT_REGEX})/$", verify_email, name="verify_email"),
-    url(r"^api/mail/weekly", send_new_users_report, name="send_new_users_report"),
+    url(r"^api/mail/weekly/$", send_new_users_report, name="send_new_users_report"),
     url(r"^api/news_signup/$", process_newsletter_form, name="process_newsletter_form"),
     url(r"^api/consent_form/$", dotmailer_consent_form, name="consent_form"),
     url(r".*", render_react, name="react_app"),
