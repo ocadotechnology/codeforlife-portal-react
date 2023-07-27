0c5c5f87ef5345872f358635f6e1faaefafcd6a2
SKairinos <skairinos@gmail.com>
2023-07-27 15:30:44 +0100

    use new cfl package

--- frontend/src/pages/register/IndependentForm.tsx
+++ frontend/src/pages/register/IndependentForm.tsx
@@ -8,17 +8,19 @@ import {
   SubmitButton,
   EmailField,
   TextField,
-  CheckboxField
+  CheckboxField,
+  DateField
 } from 'codeforlife/lib/esm/components/form';
 import { submitForm } from 'codeforlife/lib/esm/helpers/formik';
+import { MIN_DATE } from 'codeforlife/lib/esm/helpers/general';
 
 import { paths } from '../../app/router';
 import { useRegisterUserMutation } from '../../app/api';
 import BaseForm from './BaseForm';
-import DatePicker from '../../components/DatePicker';
 import CflPasswordFields from '../../features/cflPasswordFields/CflPasswordFields';
 
 interface IndependentFormValues {
+  dateOfBirth: Date;
   name: string;
   email: string;
   consentTicked: boolean;
@@ -28,6 +30,7 @@ interface IndependentFormValues {
 }
 
 const initialValues: IndependentFormValues = {
+  dateOfBirth: MIN_DATE,
   name: '',
   email: '',
   consentTicked: false,
@@ -39,17 +42,9 @@ const initialValues: IndependentFormValues = {
 const IndependentForm: React.FC = () => {
   const navigate = useNavigate();
   const [registerUser] = useRegisterUserMutation();
-  const [dateOfBirth, setDateOfBirth] = React.useState<Date>();
 
   const EmailApplicableAge = 13;
   const ReceiveUpdateAge = 18;
-
-  const yearsOfAge = dateOfBirth === undefined
-    ? undefined
-    : Math.floor(
-      (new Date().getTime() - dateOfBirth.getTime()) /
-      (1000 * 60 * 60 * 24 * 365)
-    );
 
   return (
     <BaseForm
@@ -58,96 +53,101 @@ const IndependentForm: React.FC = () => {
       description="You will have access to learning resources for Rapid Router."
       userType='independent'
     >
-      <DatePicker
-        helperText="Please enter your date of birth (we do not store this information)."
-        date={dateOfBirth}
-        onChange={(dob) => { setDateOfBirth(dob); }}
-      />
-      {yearsOfAge !== undefined && (
-        <Form
-          initialValues={initialValues}
-          onSubmit={submitForm(registerUser, {
-            build: (values) => ({
-              ...values, dateOfBirth: dateOfBirth as Date
-            }),
-            then: () => {
-              navigate(paths.register.emailVerification.independent._);
-            }
-          })}
-        >
-          <TextField
-            required
-            name="name"
-            placeholder="Full name"
-            helperText="Enter your full name"
-          />
-          <EmailField
-            required
-            placeholder="Email address"
-            helperText={
-              yearsOfAge >= EmailApplicableAge
-                ? 'Enter your email address'
-                : "Please enter your parent's email address"
-            }
-          />
-          {yearsOfAge < EmailApplicableAge && (
-            <FormHelperText style={{ fontWeight: 'bold' }}>
-              We will send your parent/guardian an email to ask them to activate
-              the account for you. Once they&apos;ve done this you&apos;ll be
-              able to log in using your name and password.
-            </FormHelperText>
-          )}
-          {yearsOfAge >= EmailApplicableAge && (
-            <CheckboxField
-              required
-              name="consentTicked"
-              formControlLabelProps={{
-                label: <>
-                  I have read and understood the &nbsp;
-                  <Link
-                    onClick={() => { navigate(paths.termsOfUse.termsOfUse._); }}
-                    target='_blank'
-                    color='inherit'
-                  >
-                    Terms of use
-                  </Link>
-                  &nbsp;and the&nbsp;
-                  <Link
-                    onClick={() => { navigate(paths.privacyNotice.privacyNotice._); }}
-                    target='_blank'
-                    color='inherit'
-                  >
-                    Privacy notice
-                  </Link>
-                  .
-                </>
-              }}
+      <Form
+        initialValues={initialValues}
+        onSubmit={submitForm(registerUser, {
+          then: () => {
+            navigate(paths.register.emailVerification.independent._);
+          }
+        })}
+      >
+        {(form) => {
+          const yearsOfAge =
+            form.values.dateOfBirth.getTime() === MIN_DATE.getTime()
+              ? undefined
+              : Math.floor(
+                (new Date().getTime() - form.values.dateOfBirth.getTime()) /
+                (1000 * 60 * 60 * 24 * 365)
+              );
+
+          return <>
+            <DateField
+              helperText="Please enter your date of birth (we do not store this information)."
+              name='dateOfBirth'
             />
-          )}
-          {yearsOfAge >= ReceiveUpdateAge && (
-            <CheckboxField
-              name="newsletterTicked"
-              formControlLabelProps={{
-                label:
-                  'Sign up to receive updates about Code for Life games and teaching resources.'
-              }}
-            />
-          )}
-          <CflPasswordFields
-            userType='independent'
-            repeatPasswordName='confirmPassword'
-          />
-          <SubmitButton
-            stackProps={{ alignItems: 'end' }}
-            endIcon={<ChevronRightIcon />}
-            disabled={(form) =>
-              !(form.dirty && form.isValid) && dateOfBirth !== undefined
-            }
-          >
-            Register
-          </SubmitButton>
-        </Form>
-      )}
+            {yearsOfAge !== undefined && <>
+              <TextField
+                required
+                name="name"
+                placeholder="Full name"
+                helperText="Enter your full name"
+              />
+              <EmailField
+                required
+                placeholder="Email address"
+                helperText={
+                  yearsOfAge >= EmailApplicableAge
+                    ? 'Enter your email address'
+                    : "Please enter your parent's email address"
+                }
+              />
+              {yearsOfAge < EmailApplicableAge && (
+                <FormHelperText style={{ fontWeight: 'bold' }}>
+                  We will send your parent/guardian an email to ask them to activate
+                  the account for you. Once they&apos;ve done this you&apos;ll be
+                  able to log in using your name and password.
+                </FormHelperText>
+              )}
+              {yearsOfAge >= EmailApplicableAge && (
+                <CheckboxField
+                  required
+                  name="consentTicked"
+                  formControlLabelProps={{
+                    label: <>
+                      I have read and understood the &nbsp;
+                      <Link
+                        onClick={() => { navigate(paths.termsOfUse.termsOfUse._); }}
+                        target='_blank'
+                        color='inherit'
+                      >
+                        Terms of use
+                      </Link>
+                      &nbsp;and the&nbsp;
+                      <Link
+                        onClick={() => { navigate(paths.privacyNotice.privacyNotice._); }}
+                        target='_blank'
+                        color='inherit'
+                      >
+                        Privacy notice
+                      </Link>
+                      .
+                    </>
+                  }}
+                />
+              )}
+              {yearsOfAge >= ReceiveUpdateAge && (
+                <CheckboxField
+                  name="newsletterTicked"
+                  formControlLabelProps={{
+                    label:
+                      'Sign up to receive updates about Code for Life games and teaching resources.'
+                  }}
+                />
+              )}
+              <CflPasswordFields
+                userType='independent'
+                repeatPasswordName='confirmPassword'
+              />
+              <SubmitButton
+                stackProps={{ alignItems: 'end' }}
+                endIcon={<ChevronRightIcon />}
+              >
+                Register
+              </SubmitButton>
+            </>}
+          </>;
+        }}
+      </Form>
     </BaseForm>
   );
 };
