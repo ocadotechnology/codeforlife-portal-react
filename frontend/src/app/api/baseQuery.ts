854376f54721b7be0269fe64415324d6e6dc849f
SKairinos <skairinos@gmail.com>
2023-08-03 12:03:49 +0300

    set default cookies in every request

--- frontend/src/app/api/baseQuery.ts
+++ frontend/src/app/api/baseQuery.ts
@@ -2,8 +2,13 @@ import {
   fetchBaseQuery,
   BaseQueryFn,
   FetchArgs,
-  FetchBaseQueryError
+  FetchBaseQueryError,
+  FetchBaseQueryMeta
 } from '@reduxjs/toolkit/query';
+import {
+  QueryReturnValue
+} from '@reduxjs/toolkit/dist/query/baseQueryTypes';
+import Cookies from 'js-cookie';
 import qs from 'qs';
 
 import {
@@ -13,15 +18,41 @@ import {
 
 import { paths } from '../router';
 
+type Result = QueryReturnValue<
+  unknown,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta
+>;
+
 const fetch = fetchBaseQuery({
   baseUrl: process.env.REACT_APP_API_BASE_URL
 });
 
-const baseQuery: BaseQueryFn<
-  FetchArgs,
-  unknown,
-  FetchBaseQueryError
-> = async (args, api, extraOptions) => {
+function setCookieRequestHeader(args: FetchArgs): void {
+  // Cookies included in every request if defined.
+  const cookie = [
+    'sessionid'
+  ].map((name) => Cookies.get(name))
+    .filter((cookie) => cookie !== undefined)
+    .join(';');
+
+  // If any cookie was defined, include it in the header.
+  if (cookie !== '') {
+    if (args.headers === undefined) {
+      args.headers = { Cookie: cookie };
+    } else if ('Cookie' in args.headers) {
+      if (args.headers.Cookie === undefined || args.headers.Cookie === '') {
+        args.headers.Cookie = cookie;
+      } else {
+        args.headers.Cookie += `;${cookie}`;
+      }
+    } else {
+      Object.defineProperty(args.headers, 'Cookie', { value: cookie });
+    }
+  }
+}
+
+function parseRequestBody(args: FetchArgs): void {
   // Check if the request has a body and its content type is specified.
   if (typeof args.body === 'object' && args.body !== null) {
     camelCaseToSnakeCase(args.body);
@@ -38,10 +69,9 @@ const baseQuery: BaseQueryFn<
       }
     }
   }
+}
 
-  // Send the HTTP request and fetch the response.
-  const result = await fetch(args, api, extraOptions);
-
+function handleResponseError(result: Result): void {
   // Handle error responses.
   if (result.error !== undefined) {
     // Parse the error's data from snake_case to camelCase.
@@ -65,11 +95,30 @@ const baseQuery: BaseQueryFn<
       }
     }
   }
+}
 
+function parseResponseBody(result: Result): void {
   // Parse the response's data from snake_case to camelCase.
   if (typeof result.data === 'object' && result.data !== null) {
     snakeCaseToCamelCase(result.data);
   }
+}
+
+const baseQuery: BaseQueryFn<
+  FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  setCookieRequestHeader(args);
+
+  parseRequestBody(args);
+
+  // Send the HTTP request and fetch the response.
+  const result = await fetch(args, api, extraOptions);
+
+  handleResponseError(result);
+
+  parseResponseBody(result);
 
   return result;
 };
