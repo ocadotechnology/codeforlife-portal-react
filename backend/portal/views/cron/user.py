5aab6c360b1b7704cfdcea893e60087e95fa4d77
faucomte97 <f.aucomte@hotmail.co.uk>
2023-09-01 17:03:13 +0100

    Shorten URLs

--- backend/portal/views/cron/user.py
+++ backend/portal/views/cron/user.py
@@ -78,6 +78,14 @@ class FirstVerifyEmailReminderView(CronMixin, APIView):
 
             sent_email_count = 0
             for email in emails:
+                email_verification_url = build_absolute_google_uri(
+                    request,
+                    reverse(
+                        "verify_email",
+                        kwargs={"token": generate_token_for_email(email)},
+                    ),
+                )
+
                 try:
                     send_email(
                         sender=NOTIFICATION_EMAIL,
@@ -85,16 +93,11 @@ class FirstVerifyEmailReminderView(CronMixin, APIView):
                         subject="Awaiting verification",
                         title="Awaiting verification",
                         text_content=USER_1ST_VERIFY_EMAIL_REMINDER_TEXT.format(
-                            email_verification_url=build_absolute_google_uri(
-                                request,
-                                reverse(
-                                    "verify_email",
-                                    kwargs={"token": generate_token_for_email(email)},
-                                ),
-                            ),
+                            email_verification_url=email_verification_url,
                             terms_url=terms_url,
                             privacy_notice_url=privacy_notice_url,
                         ),
+                        replace_url={"verify_url": email_verification_url},
                     )
 
                     sent_email_count += 1
@@ -118,6 +121,14 @@ class SecondVerifyEmailReminderView(CronMixin, APIView):
 
             sent_email_count = 0
             for email in emails:
+                email_verification_url = build_absolute_google_uri(
+                    request,
+                    reverse(
+                        "verify_email",
+                        kwargs={"token": generate_token_for_email(email)},
+                    ),
+                )
+
                 try:
                     send_email(
                         sender=NOTIFICATION_EMAIL,
@@ -125,16 +136,11 @@ class SecondVerifyEmailReminderView(CronMixin, APIView):
                         subject="Your account needs verification",
                         title="Your account needs verification",
                         text_content=USER_2ND_VERIFY_EMAIL_REMINDER_TEXT.format(
-                            email_verification_url=build_absolute_google_uri(
-                                request,
-                                reverse(
-                                    "verify_email",
-                                    kwargs={"token": generate_token_for_email(email)},
-                                ),
-                            ),
+                            email_verification_url=email_verification_url,
                             terms_url=terms_url,
                             privacy_notice_url=privacy_notice_url,
                         ),
+                        replace_url={"verify_url": email_verification_url},
                     )
 
                     sent_email_count += 1
