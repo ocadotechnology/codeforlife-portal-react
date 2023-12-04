850c134f04426236f5f7f23e4e1f330c1c14e803
Stefan Kairinos <118008817+SKairinos@users.noreply.github.com>
2023-12-04 13:11:46 +0000

    rename portal to api (#248)
    * rename portal dir to api
    
    * removed migrations and fixed paths

---
+++ backend/api/apps.py
@@ -0,0 +1,11 @@
+"""
+© Ocado Group
+Created on 04/12/2023 at 12:46:47(+00:00).
+"""
+
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
