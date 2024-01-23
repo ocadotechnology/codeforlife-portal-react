3b2c40f0505afe860a66f1a8d48d779ec209cabf
SKairinos <stefan.kairinos@ocado.com>
2024-01-23 18:25:12 +0000

    house keeping

--- backend/api/views/klass.py
+++ backend/api/views/klass.py
@@ -1,8 +1,14 @@
+"""
+© Ocado Group
+Created on 23/01/2024 at 17:53:37(+00:00).
+"""
+
 from codeforlife.user.views import ClassViewSet as _ClassViewSet
 
 from ..serializers import ClassSerializer
 
 
+# pylint: disable-next=missing-class-docstring,too-many-ancestors
 class ClassViewSet(_ClassViewSet):
     http_method_names = ["get", "post", "patch", "delete"]
     serializer_class = ClassSerializer
--- backend/api/views/school.py
+++ backend/api/views/school.py
@@ -1,8 +1,14 @@
+"""
+© Ocado Group
+Created on 23/01/2024 at 17:53:50(+00:00).
+"""
+
 from codeforlife.user.views import SchoolViewSet as _SchoolViewSet
 
 from ..serializers import SchoolSerializer
 
 
+# pylint: disable-next=missing-class-docstring,too-many-ancestors
 class SchoolViewSet(_SchoolViewSet):
     http_method_names = ["get", "post", "patch"]
     serializer_class = SchoolSerializer
