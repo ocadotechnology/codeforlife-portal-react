d567ac2b641cc53f710e7fc81906c32b0d446759
SKairinos <stefan.kairinos@ocado.com>
2024-01-30 17:03:22 +0000

    feedback

--- backend/api/serializers/student.py
+++ backend/api/serializers/student.py
@@ -3,7 +3,9 @@
 Created on 29/01/2024 at 10:14:59(+00:00).
 """
 
-from codeforlife.user.models import Class
+import typing as t
+
+from codeforlife.user.models import Class, Teacher
 from codeforlife.user.serializers import StudentSerializer as _StudentSerializer
 from rest_framework import serializers
 
@@ -17,25 +19,22 @@ class StudentSerializer(_StudentSerializer):
 
     # pylint: disable-next=missing-function-docstring
     def validate_klass(self, value: str):
-        if self.request_user.teacher is None:
-            raise serializers.ValidationError(
-                "Only a teacher can assign a student to class."
-            )
-        if self.request_user.teacher.school is None:
+        # Only teachers can manage students.
+        teacher = t.cast(Teacher, self.request_user.teacher)
+
+        if teacher.school is None:
             raise serializers.ValidationError(
                 "The requesting teacher must be in a school."
             )
 
         queryset = Class.objects.filter(access_code=value)
-        if not queryset.filter(
-            teacher__school=self.request_user.teacher.school_id,
-        ).exists():
+        if not queryset.filter(teacher__school=teacher.school_id).exists():
             raise serializers.ValidationError(
                 "Class must belong to the same school as requesting teacher."
             )
         if (
-            not self.request_user.teacher.is_admin
-            and not queryset.filter(teacher=self.request_user.teacher).exists()
+            not teacher.is_admin
+            and not queryset.filter(teacher=teacher).exists()
         ):
             raise serializers.ValidationError(
                 "The requesting teacher must be an admin or own the class."
