4e8e8b8a5ad6783333cc57feac650951bd1b0a46
SKairinos <stefan.kairinos@ocado.com>
2024-02-05 14:46:41 +0000

    create school

--- backend/api/views/school.py
+++ backend/api/views/school.py
@@ -3,6 +3,10 @@
 Created on 23/01/2024 at 17:53:50(+00:00).
 """
 
+import typing as t
+
+from codeforlife.permissions import NOT, AllowNone, Permission
+from codeforlife.user.permissions import InSchool, IsTeacher
 from codeforlife.user.views import SchoolViewSet as _SchoolViewSet
 
 from ..serializers import SchoolSerializer
@@ -12,3 +16,16 @@ from ..serializers import SchoolSerializer
 class SchoolViewSet(_SchoolViewSet):
     http_method_names = ["get", "post", "patch"]
     serializer_class = SchoolSerializer
+
+    def get_permissions(self):
+        # Bulk actions not allowed for schools.
+        if self.action in ["bulk", "list"]:
+            return [AllowNone()]
+        # Only teachers not in a school can create a school.
+        if self.action == "create":
+            return [IsTeacher(), NOT(InSchool())]
+        # Only admin-teachers in a school can update a school.
+        if self.action == "update":
+            return [IsTeacher(is_admin=True), InSchool()]
+
+        return [InSchool()]
