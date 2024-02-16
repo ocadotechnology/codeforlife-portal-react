2c6df754444f06df9d53a4b7f46699bb65ce8dfb
SKairinos <stefan.kairinos@ocado.com>
2024-02-16 14:40:53 +0000

    new python package

--- backend/api/tests/views/test_klass.py
+++ backend/api/tests/views/test_klass.py
@@ -20,31 +20,24 @@ class TestClassViewSet(ModelViewSetTestCase[Class]):
     model_view_set_class = ClassViewSet
     fixtures = ["school_1"]
 
-    def test_get_permissions__bulk(self):
-        """
-        No one is allowed to perform bulk actions.
-        """
+    # test: get permissions
 
+    def test_get_permissions__bulk(self):
+        """No one is allowed to perform bulk actions."""
         self.assert_get_permissions(
             permissions=[AllowNone()],
             action="bulk",
         )
 
     def test_get_permissions__create(self):
-        """
-        Only a school-teacher can create a class.
-        """
-
+        """Only school-teachers can create a class."""
         self.assert_get_permissions(
             permissions=[IsTeacher(in_school=True)],
             action="create",
         )
 
     def test_get_permissions__update(self):
-        """
-        Only an admin- or class-teacher can update a class.
-        """
-
+        """Only admin-teachers or class-teachers can update a class."""
         self.assert_get_permissions(
             permissions=[
                 OR(IsTeacher(is_admin=True), IsTeacher(in_class=True))
@@ -53,10 +46,7 @@ class TestClassViewSet(ModelViewSetTestCase[Class]):
         )
 
     def test_get_permissions__destroy(self):
-        """
-        Only an admin- or class-teacher can destroy a class.
-        """
-
+        """Only admin-teachers or class-teachers can destroy a class."""
         self.assert_get_permissions(
             permissions=[
                 OR(IsTeacher(is_admin=True), IsTeacher(in_class=True))
@@ -65,22 +55,25 @@ class TestClassViewSet(ModelViewSetTestCase[Class]):
         )
 
     def test_get_permissions__list(self):
-        """
-        Only a school-teacher can list classes.
-        """
-
+        """Only admin-teachers and class-teachers can list classes."""
         self.assert_get_permissions(
-            permissions=[IsTeacher(in_school=True)],
+            permissions=[
+                OR(IsTeacher(is_admin=True), IsTeacher(in_class=True))
+            ],
             action="list",
         )
 
     def test_get_permissions__retrieve(self):
         """
-        Any school-user can retrieve a class.
+        Only students, admin-teachers or class-teachers can retrieve a class.
         """
-
         self.assert_get_permissions(
-            permissions=[OR(IsStudent(), IsTeacher(in_school=True))],
+            permissions=[
+                OR(
+                    IsStudent(),
+                    OR(IsTeacher(is_admin=True), IsTeacher(in_class=True)),
+                )
+            ],
             action="retrieve",
         )
 
